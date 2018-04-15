40fe1673bfa0e793f20cec52f9180693d7c267c0
kenorb <kenorb@users.noreply.github.com>
2018-04-15 21:00:13 +0100

    Configure proxychains.conf

--- scripts/get_proxy_list.sh
+++ scripts/get_proxy_list.sh
@@ -3,15 +3,5 @@
 repo="kenorb/proxy_list"
 ci_api="travis-ci.2.2+json"
 ip_url="ifconfig.co"
-
 job_id=$(jq ".builds|max_by(.id)|.job_ids|max_by(.)" < <(curl -H "Accept: application/vnd.${ci_api}" -sL https://api.travis-ci.org/repos/$repo/builds))
-egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,5}' < <(curl -sL https://api.travis-ci.org/jobs/$job_id/log) | \
-  while IFS=: read ip port; do
-    (
-      (timeout 2 curl -x socks5://$ip:$port -qs -o/dev/null http://example.com/ && printf "socks5\t$ip\t$port\n") \
-      || (timeout 2 curl -x socks4://$ip:$port -qs -o/dev/null http://example.com/ && printf "socks4\t$ip\t$port\n") \
-      || (timeout 2 curl -x http://$ip:$port -qs -o/dev/null http://example.com/ && printf "http\t$ip\t$port\n")
-    ) &
-    sleep 0.2
-  done
-wait
+egrep -w "^(socks4|socks5|http)" < <(curl -sL https://api.travis-ci.org/jobs/$job_id/log)
